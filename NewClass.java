f922afea9ceb434f29283787c75250f6840f6897
amliagstn <150033537+amliagstn@users.noreply.github.com>
2024-02-07 21:52:32 +0700

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inputankeyboard;
+import java.util.Scanner;
+public class NewClass {
+    public static void main (String[] args) {
+        //Deklarasi variable
+        String fName = "Amelia";
+        String IName = "Agustin";
+        String stuId = "E41230432";
+        String stuStatus = "Active";
+        
+        //Menampilkan informasi siswa
+        System.out.println("Student Name: " + fName + " " + IName);
+        System.out.println("Stydent ID: " + stuId);
+        System.out.println("Student Status: " + stuStatus);
+    }
+}
